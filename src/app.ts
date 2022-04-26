53595143b7456573d49735f9bedcd1d2c0e9c006
HitzSPB <daniel.oesterbro@gmail.com>
2022-04-26 17:23:47 +0200

    Basic TS

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Test');
+function add(number1: number, number2: number) {
+    return number1 + number2;
+  }
+  
+  const int1 = 5;
+  const int2 = 2.8;
+  
+  const result = add(int1, int2);
+  console.log(result);
